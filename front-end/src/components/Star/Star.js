9b7d08df6f587a698f59d5b65d38cb1fa668d2c5
robert-juang <zdjiang20001225@gmail.com>
2023-10-19 13:55:59 -0400

    replaced files to better illustrate structure

---
+++ front-end/src/components/Star/Star.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import "./Star.css"
+
+function Star() {
+  return (
+    <div className="Star-main">This is a star component</div>
+  )
+}
+
+export default Star
---
+++ front-end/src/context/ThemeContext.js
@@ -0,0 +1,5 @@
+//Create a new context if you see fit and then export it. For example: 
+
+import {createContext} from 'react'; 
+
+export const ThemeContext = createContext(null)
