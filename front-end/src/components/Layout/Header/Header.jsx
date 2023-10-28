7c4730474ab87280de29a6047635fc44a3c3e445
robert-juang <zdjiang20001225@gmail.com>
2023-10-28 14:43:46 -0400

    mobile styling

--- front-end/src/components/Layout/Header/Header.jsx
+++ front-end/src/components/Layout/Header/Header.jsx
@@ -1,15 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Header.css"
 import { Outlet } from "react-router-dom";
+
+import Hamburger from "hamburger-react"
 
 //TODO: add number after the profile route and manga route 
 
 export default function Header() {
+    
+    const [isOpen, setOpen] = useState(false); 
+
     return (
         <>
             <div className="Header-sidebar">
                 <h1>Manga Bookshelf for the Nerds</h1>
                 <div>
+                    <div className="Header-navigation-sm">
+                        <Hamburger toggled={isOpen} toggle={setOpen} />
+                    </div>
                     <form id="search-form" role="search">
                         <input
                             id="q"
@@ -31,23 +39,46 @@ export default function Header() {
                     <form method="post">
                         <button type="submit">Submit</button>
                     </form>
+
                 </div>
-                <nav>
-                    <ul>
-                        <li>
-                            <a href={`/dashboard`}>Dashboard</a>
-                        </li>
-                        <li>
-                            <a href={`/manga`}>Mangas</a>
-                        </li>
-                        <li>
-                            <a href={`/profile/1`}>My Profile</a>
-                        </li>
-                        <li>
-                            <a href={`/setting`}>Setting</a>
-                        </li>
-                    </ul>
-                </nav>
+                <div className="Header-nav">
+                    <nav>
+                        <ul>
+                            <li>
+                                <a href={`/dashboard`}>Dashboard</a>
+                            </li>
+                            <li>
+                                <a href={`/manga`}>Mangas</a>
+                            </li>
+                            <li>
+                                <a href={`/profile/1`}>My Profile</a>
+                            </li>
+                            <li>
+                                <a href={`/setting`}>Setting</a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+                <div className="Header-navigation-sm">
+                    {isOpen ? (
+                        <nav>
+                            <ul>
+                                <li>
+                                    <a href={`/dashboard`}>Dashboard</a>
+                                </li>
+                                <li>
+                                    <a href={`/manga`}>Mangas</a>
+                                </li>
+                                <li>
+                                    <a href={`/profile/1`}>My Profile</a>
+                                </li>
+                                <li>
+                                    <a href={`/setting`}>Setting</a>
+                                </li>
+                            </ul>
+                        </nav>
+                    ) : <div></div>}
+                </div>
             </div>
             <div id="detail">
                 <Outlet />
