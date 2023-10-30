4186df83b890edfbf911d85e5996ce6b6bb028d4
Gloria Zhu <85907267+gzhu725@users.noreply.github.com>
2023-10-30 14:25:09 -0400

    add forgot password and sign up links

--- front-end/src/pages/Auth/LogIn/login.jsx
+++ front-end/src/pages/Auth/LogIn/login.jsx
@@ -16,6 +16,12 @@ function login() {
             <label>Password:</label>
             <input type="password" id="password" name="password" placeholder="Enter your password" required />
         </div>
+        <div>
+            <a class="form-group" href="https://www.google.com/">New? Sign Up!</a>
+            <br></br>
+            <a class ="form-group" href="https://www.gmail.com">Forgot password?</a> 
+        </div>
+
         <div class="form-group">
             <button onClick={window.history.replaceState("", "", "/")}>Login</button>
         </div>
