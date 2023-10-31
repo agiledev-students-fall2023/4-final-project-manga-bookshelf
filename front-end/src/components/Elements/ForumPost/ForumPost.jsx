056b3ef11c352f1ed1ea2cf758489f2ec5eac39f
cmtsao <cmntsao@gmail.com>
2023-10-30 23:41:10 -0400

    Fixed css class labeling for ForumPost

--- front-end/src/components/Elements/ForumPost/ForumPost.jsx
+++ front-end/src/components/Elements/ForumPost/ForumPost.jsx
@@ -5,46 +5,31 @@ import "./ForumPost.css"
 function ForumPost({username}){
     const user= username
     return (
-        <div class= "forum">
-            {/* <div class = "username">
-                <label for= "User">
-                </label>
-                <
-            </div>
-            <div class ="entry">
-            <label> Comment </label>
-                <input 
-                    type="text"
-                    id="comment"
-                    name="comment" 
-                    placeholder="Write your comment here"
-                />
-            </div> */}
-            <form class="comment-entry">
-                <div class="row">
-                    <div class="col-labels">
+        <div class= "ForumPost-forum">
+            <form class="ForumPost-comment-entry">
+                <div class="ForumPost-row">
+                    <div class="ForumPost-col-labels">
                         <label for="username"> Username </label>
                     </div>
-                    <div class="col-entries">
+                    <div class="ForumPost-col-entries">
                         <p>{user}</p>
                     </div>
                 </div>
-                <div class="row">
-                    <div class="col-labels">
+                <div class="ForumPost-row">
+                    <div class="ForumPost-col-labels">
                         <label for="comment"> Comment </label>
                     </div>
-                    <div class="col-entries">
+                    <div class="ForumPost-col-entries">
                         <textarea 
-                            // type= "forum"
                             id="comment"
                             name="comment" 
                             placeholder="Write your comment here"
-                            style={{ minHeight: '200px' , minWidth: '100%'}}>
+                            style={{ minHeight: '75px' , minWidth: '100%'}}>
                         </textarea>
                     </div>
                 </div>
                 <br></br>
-                <div class="row">
+                <div class="ForumPost-row">
                     <input 
                         type="submit" 
                         value="Submit"
