1a132d524e49de68c7b722d5d741a853de122637
robert-juang <zdjiang20001225@gmail.com>
2023-11-09 13:03:44 -0500

    saved before pulling from main

--- front-end/src/components/Layout/Header/Header.jsx
+++ front-end/src/components/Layout/Header/Header.jsx
@@ -14,6 +14,33 @@ import Turnstone from 'turnstone'
 export default function Header() {
     
     const [isOpen, setOpen] = useState(false); 
+    const [searchBox, setSearchBox] = useState([]) 
+    //Define constant styles to use Turnstone 
+
+    const maxItems = 10
+
+    const listbox = [
+        {
+            id: 'mangas',
+            name: 'Mangas',
+            ratio: 6,
+            displayField: 'manga',
+            data: (query) =>
+                fetch(`localhost:8080/search/${query}`)
+                    .then(res => res.json()),
+            searchType: 'startswith'
+        }
+        // {
+        //     id: 'users',
+        //     name: 'Users',
+        //     ratio: 4,
+        //     displayField: 'user',
+        //     data: (query) =>
+        //         fetch(`/api/airports?q=${encodeURIComponent(query)}&limit=10`)
+        //             .then(res => res.json()),
+        //     searchType: 'contains'
+        // }
+    ]
 
     return (
         <>
@@ -25,13 +52,18 @@ export default function Header() {
                         <Hamburger toggled={isOpen} toggle={setOpen} />
                     </div>
                     <form id="search-form" role="search">
-                        <input
+                        {/* <input
                             id="q"
                             aria-label="Search contacts"
                             placeholder="Search"
                             type="search"
                             name="q"
-                        />
+                        /> */}
+                            <Turnstone
+                                listbox={listbox}
+                                cancelButton={true}
+
+                            />
                     </form>
                     <form method="post">
                         <button type="submit">Submit</button>
