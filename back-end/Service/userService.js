071461871922383fc21b4c7543003a3e90790647
ltaoming <tl3109@nyu.edu>
2023-11-13 16:49:45 -0500

    add some unit tests for User related functionalities

--- back-end/Service/userService.js
+++ back-end/Service/userService.js
@@ -3,32 +3,32 @@ import fs from "fs";
 
 class UserService {
   /* Create a new user, mainly for log in */
-  async createUser(data) {}
+  // async createUser(data) {}
 
   /* Get user data from database - need to update when connect to database */
   async getUserData(userId) {
     const user = userMockData.users.find((user) => user.id === userId);
     if (!user) {
-      return res.status(404).send({ message: "User not found." });
+      throw new Error('User not found.');
     }
     return user;
   }
 
   /* Update data to database. Only for backend without database */
   /* Don't use it yet. It is not working */
-  async updateUserData(userData) {
-    try {
-      userMockData.users.push(userData);
-      const dataString =
-        typeof userMockData === "string"
-          ? userMockData
-          : JSON.stringify(userMockData, null, 2);
-      await fs.promises.writeFile("./public/userMockData.json", dataString);
-    } catch (err) {
-      console.error("Could not save users mock data:", err);
-      throw err;
-    }
-  }
+  // async updateUserData(userData) {
+  //   try {
+  //     userMockData.users.push(userData);
+  //     const dataString =
+  //       typeof userMockData === "string"
+  //         ? userMockData
+  //         : JSON.stringify(userMockData, null, 2);
+  //     await fs.promises.writeFile("./public/userMockData.json", dataString);
+  //   } catch (err) {
+  //     console.error("Could not save users mock data:", err);
+  //     throw err;
+  //   }
+  // }
 }
 
 export default new UserService();
