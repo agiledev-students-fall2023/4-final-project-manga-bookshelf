45e31f42be4b5bf63e99fdbb403d2086dc61d4cd
gzhu725 <gloriazhu66@gmail.com>
2023-11-13 13:26:37 -0500

    Revert "save change"
    This reverts commit 41ab209d189688f74d2a43e873fac9b54a61e4b6.

--- front-end/src/pages/Follow/follow.jsx
+++ front-end/src/pages/Follow/follow.jsx
@@ -5,6 +5,31 @@ import { BsArrowLeft } from 'react-icons/bs'
 
 import './follow.css'
 
+// // This is for mocking purposes only
+// const follower = [
+//     { id: 1, name: "John Doe", avatar: "url_to_avatar_1" },
+//     { id: 2, name: "Jane Smith", avatar: "url_to_avatar_1" },
+//     { id: 3, name: "John Doe", avatar: "url_to_avatar_1" },
+//     { id: 4, name: "Jane Smith", avatar: "url_to_avatar_1" },
+//     { id: 5, name: "John Doe", avatar: "url_to_avatar_1" },
+//     { id: 6, name: "Jane Smith", avatar: "url_to_avatar_1" },
+//     { id: 7, name: "John Doe", avatar: "url_to_avatar_1" },
+//     { id: 8, name: "Jane Smith", avatar: "url_to_avatar_1" },
+//     { id: 9, name: "John Doe", avatar: "url_to_avatar_1" },
+//     { id: 10, name: "Jane Smith", avatar: "url_to_avatar_1" },
+//     { id: 11, name: "John Doe", avatar: "url_to_avatar_1" },
+//     { id: 12, name: "Jane Smith", avatar: "url_to_avatar_1" },
+//     { id: 13, name: "Jane Smith", avatar: "url_to_avatar_1" },
+//     { id: 14, name: "Jane Smith", avatar: "url_to_avatar_1" },
+    
+//     // ... Add more users as needed
+// ];
+// const following = [
+//     { id: 1, name: "Sam Smith", avatar: "url_to_avatar_1" },
+//     { id: 2, name: "Joe", avatar: "url_to_avatar_2" },
+//     // ... Add more users as needed
+// ];
+
 function Follow({ title }) {
     const navigate = useNavigate()
     const { profileId } = useParams()
@@ -15,7 +40,7 @@ function Follow({ title }) {
     }
 
     const fetchFollowers = useCallback(async () => {
-        const response = await fetch(`http://localhost:8080/user/${profileId}/follower`);
+        const response = await fetch(`http://localhost:8080/user/${profileId}/followers`);
         const data = await response.json();
         setFollowers(data);
     }, [profileId]);
