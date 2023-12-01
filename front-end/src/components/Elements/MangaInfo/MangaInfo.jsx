d74eea9750441a22477dbdba8f4dd2248878aae0
cmtsao <cmntsao@gmail.com>
2023-12-01 04:28:20 -0500

    Added functionality to add manga to reading list

--- front-end/src/components/Elements/MangaInfo/MangaInfo.jsx
+++ front-end/src/components/Elements/MangaInfo/MangaInfo.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react"
 import MangaProfileImage from "../MangaProfileImage/MangaProfileImage"
 import ForumPost from "../ForumPost/ForumPost"
+import { isFavorite } from "../../../helper/helper"
+
 
 import "./MangaInfo.css"
 
@@ -14,15 +16,14 @@ function MangaInfo({mangaData}) {
     const [chapter, setChapter] = useState('')
     const [isMenuOpen, setMenuOpen] =useState(false)
     const [user, setUser] = useState({})
+    const [isLoading, setIsLoading] = useState(true) 
+
+    const [reading, setReading] = useState(false)
 
     const handleAddClick = () => {
         setMenuOpen(!isMenuOpen)
     }
 
-    const handleAddListClick = (item) => {
-        console.log(`clicked on ${item}`)
-        setMenuOpen(false)
-    }
 
     const handleChapterChange = (e) =>{
         const input = e.target.value
@@ -31,21 +32,87 @@ function MangaInfo({mangaData}) {
     }
 
       //retrieve all the mangaList items from user and pass into MangaIcon
-    useEffect(() => {
-        async function getData(){
-            const myHeaders = new Headers();
+    // useEffect(() => {
+    //     async function getData(){
+    //         const myHeaders = new Headers();
         
-            myHeaders.append('Content-Type', 'application/json');
-            myHeaders.append('Authorization', `Bearer ${localStorage.getItem("jwtToken")}`);
+    //         myHeaders.append('Content-Type', 'application/json');
+    //         myHeaders.append('Authorization', `Bearer ${localStorage.getItem("jwtToken")}`);
         
-            const response3 = await fetch(`${process.env.REACT_APP_BACKEND_URL}/protected/user/get/currentuser/`, {
-            method: "GET",
-            headers: myHeaders
+    //         const response3 = await fetch(`${process.env.REACT_APP_BACKEND_URL}/protected/user/get/currentuser/`, {
+    //         method: "GET",
+    //         headers: myHeaders
+    //         })
+    //         const data3 = await response3.json()
+    //         setUser(data3.user)
+    //         setIsLoading(false) 
+    //     }
+    //     getData() 
+    // }, [])
+
+    // if (isLoading) {
+    //     return <div>Loading...</div>; // Or any other loading indicator
+    //   }
+
+    const handleAddListClick = (item) => {
+        console.log(`clicked on ${item}`)
+        setMenuOpen(false)
+    }
+
+    const handleReadingClick = async () => {
+        //define headers 
+        const myHeaders = new Headers();
+        
+        myHeaders.append('Content-Type', 'application/json');
+        myHeaders.append('Authorization', `Bearer ${localStorage.getItem("jwtToken")}`);
+        
+        //if current manga is in reading 
+        if (reading){ 
+          try { 
+            const response3 = await fetch(`${process.env.REACT_APP_BACKEND_URL}/protected/user/delete/currentlyreading/${__id}`, {
+              method: "DELETE",
+              headers: myHeaders
             })
             const data3 = await response3.json()
-            setUser(data3.user)
+            console.log(data3) 
+          } catch (error) {
+            console.error("Error fetching or accessing db", error)
+          }
         }
-        getData() 
+        else{ // if current manga is not in reading 
+        //   const mangaData = 
+        //   {
+        //     title: name, 
+        //     image: imgLink, 
+        //     __id: mangaId, 
+        //     authorName: "N/A please fix", 
+        //     authorImage: "N/A please fix"
+        //   }
+          
+          try {
+            const response3 = await fetch(`${process.env.REACT_APP_BACKEND_URL}/protected/user/add/currentlyreading`, {
+              method: "POST",
+              headers: myHeaders, 
+              body: JSON.stringify(mangaData)
+            })
+            const data3 = await response3.json()
+            console.log(data3) 
+          } catch (error) {
+            console.error("Error fetching or accessing db", error)
+          }
+        }
+        setReading(!reading); 
+        console.log(user)
+      }
+      //determine if the manga is currently reading or not 
+    useEffect(() => {
+        console.log(user)
+        if (isFavorite(user["currentlyReading"], __id)){
+            console.log("true in reading")
+            setReading(true) 
+        }else{
+            setReading(false)
+        }
     }, [])
 
     return (
@@ -58,7 +125,7 @@ function MangaInfo({mangaData}) {
                         <button onClick={handleAddClick}>+ Add to List</button>
                             {isMenuOpen && (
                                 <ul className="menu">
-                                    <button onClick={() => handleAddListClick('Reading')}>Reading</button>
+                                    <button onClick={() => handleReadingClick('Reading')}>Reading</button>
                                     <button onClick={() => handleAddListClick('Want to Read')}>Want to Read</button>
                                     <button onClick={() => handleAddListClick('Already Read')}>Already Read</button>
                                 </ul>
