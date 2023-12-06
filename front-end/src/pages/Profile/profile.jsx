a50e0cc492946f79f6bf89aede7febfbd3105363
ltaoming <tl3109@nyu.edu>
2023-12-06 00:54:17 -0500

    handle non-exist users

--- front-end/src/pages/Profile/profile.jsx
+++ front-end/src/pages/Profile/profile.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
@@ -10,126 +10,164 @@ const titles = ["Currently Reading", "Done", "Want to Read"];
 function Profile() {
   const [profileLists, setProfileLists] = useState([]);
   const [profileInfo, setProfileInfo] = useState({});
-  const { profileId } = useParams()
-  const currentUser = JSON.parse(localStorage.getItem('user')).username
-  const isCurrentUser = currentUser === profileId
-  const [isFollowed, setIsFollowed] = useState(false)
-  const [loading, setLoading] = useState(false)
+  const { profileId } = useParams();
+  const currentUser = JSON.parse(localStorage.getItem("user")).username;
+  const isCurrentUser = currentUser === profileId;
+  const [isFollowed, setIsFollowed] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [userExists, setUserExists] = useState(true);
 
   const handleFollowClick = () => {
     if (loading) {
-      return
+      return;
     }
-    setLoading(true)
-    const actionUrl = isFollowed 
-      ? `${process.env.REACT_APP_BACKEND_URL}/user/${currentUser}/unfollow` 
-      : `${process.env.REACT_APP_BACKEND_URL}/user/${currentUser}/follow`
+    setLoading(true);
+    const actionUrl = isFollowed
+      ? `${process.env.REACT_APP_BACKEND_URL}/user/${currentUser}/unfollow`
+      : `${process.env.REACT_APP_BACKEND_URL}/user/${currentUser}/follow`;
 
     const payload = isFollowed
       ? { unfollowingName: profileInfo.username }
-      : { followingName: profileInfo.username }
+      : { followingName: profileInfo.username };
 
-    axios.post(actionUrl, payload)
-      .then(response => {
-        setIsFollowed(!isFollowed)
+    axios
+      .post(actionUrl, payload)
+      .then((response) => {
+        setIsFollowed(!isFollowed);
       })
-      .catch(err => {
-        console.error(`Error in ${isFollowed ? 'unfollow' : 'follow'} request: `, err)
+      .catch((err) => {
+        console.error(
+          `Error in ${isFollowed ? "unfollow" : "follow"} request: `,
+          err
+        );
       })
       .finally(() => {
-        setLoading(false)
-      })
-    }
+        setLoading(false);
+      });
+  };
 
-    const groupListsByTitle = (title) => {
-      const filteredLists = profileLists.map((profile) => ({
-        result: profile.result.filter((item) => item.list === title),
-      }));
-  
-      return filteredLists;
-    };
+  const groupListsByTitle = (title) => {
+    const filteredLists = profileLists.map((profile) => ({
+      result: profile.result.filter((item) => item.list === title),
+    }));
+
+    return filteredLists;
+  };
 
   //get a list of the users profile lists (mock data)
   useEffect(() => {
     async function getProfileLists() {
-      const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}/getProfileLists`);
+      const response = await fetch(
+        `${process.env.REACT_APP_BACKEND_URL}/getProfileLists`
+      );
       const data = await response.json();
       setProfileLists([data.result]);
     }
 
     async function getUserInfo() {
       const myHeaders = new Headers();
-      
-      myHeaders.append('Content-Type', 'application/json');
-      myHeaders.append('Authorization', `Bearer ${localStorage.getItem("jwtToken")}`);
 
-      const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}/protected/user/get/anotheruser/${profileId}`, {
-        method: "GET",
-        headers: myHeaders
-      })
-      const data = await response.json()
-      setProfileInfo(data)
+      myHeaders.append("Content-Type", "application/json");
+      myHeaders.append(
+        "Authorization",
+        `Bearer ${localStorage.getItem("jwtToken")}`
+      );
+
+      const response = await fetch(
+        `${process.env.REACT_APP_BACKEND_URL}/protected/user/get/anotheruser/${profileId}`,
+        {
+          method: "GET",
+          headers: myHeaders,
+        }
+      );
+
+      const data = await response.json();
+      if (!response.ok || data == null) {
+        setUserExists(false);
+        return;
+      }
+      setProfileInfo(data);
       if (!isCurrentUser) {
-        setIsFollowed(data.follower.includes(JSON.parse(localStorage.getItem('user')).username));
+        setIsFollowed(
+          data.follower.includes(
+            JSON.parse(localStorage.getItem("user")).username
+          )
+        );
       }
     }
-    getUserInfo()
+    getUserInfo();
     getProfileLists();
   }, [profileId, isCurrentUser]);
 
-
   return (
-    <main className="profile-main">
-      <div className="profile-contact">
-        <div className="profile-image">
-          <img
-            src={profileInfo.profileImg}
-            alt="No Img Detected"
-          />
-        </div>
+    <>
+      {!userExists ? (
+        <h1>User does not exist</h1>
+      ) : (
+        <main className="profile-main">
+          <div className="profile-contact">
+            <div className="profile-image">
+              <img src={profileInfo.profileImg} alt="No Img Detected" />
+            </div>
 
-        <div className="profile-bio">
-          <h1>
-            Welcome, {profileInfo.username ? <>{profileInfo.username}</> : <i>No Name</i>}{" "}
-          </h1>
+            <div className="profile-bio">
+              <h1>
+                Welcome,{" "}
+                {profileInfo.username ? (
+                  <>{profileInfo.username}</>
+                ) : (
+                  <i>No Name</i>
+                )}{" "}
+              </h1>
 
-          <div className="follow-section">
-            <Link to={`/profile/${profileId}/follower`} activeclassname="current">
-              <button type="submit">Follower</button>
-            </Link>
-            <Link to={`/profile/${profileId}/following`} activenlassname="current">
-              <button type="submit">Following</button>
-            </Link>
+              <div className="follow-section">
+                <Link
+                  to={`/profile/${profileId}/follower`}
+                  activeclassname="current"
+                >
+                  <button type="submit">Follower</button>
+                </Link>
+                <Link
+                  to={`/profile/${profileId}/following`}
+                  activenlassname="current"
+                >
+                  <button type="submit">Following</button>
+                </Link>
+              </div>
+
+              {profileInfo.bio && <p>{profileInfo.bio}</p>}
+            </div>
+
+            <div className="edit-section">
+              {!isCurrentUser && (
+                <button
+                  className={`follow-button ${
+                    isFollowed ? "followed" : "not-followed"
+                  }`}
+                  onClick={handleFollowClick}
+                >
+                  {isFollowed ? "Unfollow" : "Follow +"}
+                </button>
+              )}
+              {isCurrentUser && (
+                <Link
+                  to={`/profile/${profileId}/edit`}
+                  activeclassname="current"
+                >
+                  <button type="submit">Edit Profile</button>
+                </Link>
+              )}
+            </div>
           </div>
 
-          {profileInfo.bio && <p>{profileInfo.bio}</p>}
-        </div>
-
-        <div className="edit-section">
-          { !isCurrentUser && (
-              <button 
-                className={`follow-button ${isFollowed ? 'followed' : 'not-followed'}`}
-                onClick={handleFollowClick}
-              >
-                { isFollowed ? 'Unfollow' : 'Follow +' }
-              </button>
-          )}
-          { isCurrentUser && (
-              <Link to={`/profile/${profileId}/edit`} activeclassname="current">
-                <button type="submit">Edit Profile</button>
-              </Link>
-            )
-          }
-
-        </div>
-      </div>
-
-      <section className="myList">
-        {titles.map((t) => (
-          <MangaRow title={t} MangaList={groupListsByTitle(t)} />
-        ))}
-      </section>
-    </main>
+          <section className="myList">
+            {titles.map((t) => (
+              <MangaRow title={t} MangaList={groupListsByTitle(t)} />
+            ))}
+          </section>
+        </main>
+      )}
+    </>
   );
 }
 export default Profile;
